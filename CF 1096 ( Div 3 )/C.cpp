8ca7e3c24d0893a58a6ee4604eec967e9ed6b333
Apurba Chandra Kabiraj <apkabiraj11@gmail.com>
2026-05-01 00:05:03 +0600

    Updated 30 april, 2026

---
+++ CF 1096 ( Div 3 )/C.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+
+#define ll long long int
+#define ln '\n'
+#define yes cout << "YES" << ln
+#define no cout << "NO" << ln
+const int N = 1e6 + 9, mod = 1e9 + 7;
+
+// template <typename T>
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// Functions:
+// 1. find_by_order(k) -> returns iterator to the k-th element (0-indexed)
+// 2. order_of_key(x) -> returns number of elements strictly smaller than x
+
+
+void solve() {
+    int n;
+    cin>> n;
+    vector<int>ar(n);
+    vector<int>a, b, c, d;
+    for(int i = 0; i < n; i++) {
+    	cin >> ar[i];
+    	if(ar[i] % 6 == 0)
+    		d.push_back(ar[i]);
+    	else if(ar[i] % 2 == 0)
+    		a.push_back(ar[i]);
+    	else if(ar[i] % 3 == 0)
+    		b.push_back(ar[i]);
+    	else c.push_back(ar[i]);
+
+    }
+    ar.clear();
+    for(auto y : a)
+    	ar.push_back(y);
+    for(auto x : c)
+    	ar.push_back(x);
+    for(auto x : b)
+    	ar.push_back(x);
+    for(auto x : d)
+    	ar.push_back(x);
+    
+
+    for(auto x : ar)
+    	cout << x << " ";
+    cout << ln;
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    // int casee = 1;
+    while(t--) {
+        // cout << "Case " << casee << ": ";
+        solve();
+        // casee++;
+    }
+
+    return 0;
+}
+
+/**************
+
+LEARNING IS FUN
+SO ENJOY IT 
+
+***************/
+
