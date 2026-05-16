62b88bf5be87126fe2e0f56704e98a43466f8dbc
Apurba Chandra Kabiraj <apkabiraj11@gmail.com>
2026-05-17 01:16:12 +0600

    Updated 17 may, 2026

---
+++ CF 1098 ( DIV 2 )/A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+
+#define ll long long int
+#define ln '\n'
+#define yes cout << "YES" << ln
+#define no cout << "NO" << ln
+const int N = 1e6 + 9, mod = 1e9 + 7;
+
+// template <typename T>
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// Functions:
+// 1. find_by_order(k) -> returns iterator to the k-th element (0-indexed)
+// 2. order_of_key(x) -> returns number of elements strictly smaller than x
+
+
+void solve() {
+   	int n;
+   	cin >> n;
+   	vector<int>ar(n);
+   	vector<int>freq(3);
+   	for(int i = 0; i < n; i++){
+   		cin >> ar[i];
+   		freq[ar[i]]++;
+   	}
+   	int ans = 0;
+   	ans += freq[0];
+
+   	int temp = min(freq[1], freq[2]);
+   	ans += temp;
+
+   	freq[1] -= temp;
+   	freq[2] -= temp;
+   	ans += freq[1] / 3;
+   	ans += freq[2] / 3;
+   	cout << ans << ln;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    // int casee = 1;
+    while(t--) {
+        // cout << "Case " << casee << ": ";
+        solve();
+        // casee++;
+    }
+
+    return 0;
+}
+
+/**************
+
+LEARNING IS FUN
+SO ENJOY IT 
+
+***************/
+
